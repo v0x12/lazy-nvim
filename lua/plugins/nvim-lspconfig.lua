0486b546df603b325ce262645257f241b91c1626
v0x12 <olahnorbert1997@gmail.com>
2023-12-13 10:35:03 +0100

    Configuring language servers and formatters

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,104 @@
+local on_attach = require("util.lsp").on_attach
+
+local config = function()
+	require("neoconf").setup({})
+
+	local lspconfig = require("lspconfig")
+
+	local signs = { Error = "", Warn = "!", Hint = "", Info = "" }
+
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
+
+
+	lspconfig.lua_ls.setup({
+		-- capabilities = capabilities,
+		on_attach = on_attach,
+		settings = {
+			Lua = {
+				-- make the language server recognize "vim" global
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					-- make language server aware of runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.stdpath("config") .. "/lua"] = true,
+					},
+				},
+			},
+		},
+	})
+
+	lspconfig.pyright.setup({
+		-- capabilities = capabilities,
+		on_attach = on_attach,
+		settings = {
+			pyright = {
+				disableOrganizeImports = false,
+				analysis = {
+					useLibraryCodeForTypes = true,
+					autoSearchPaths = true,
+					diagnosticMode = "workspace",
+					autoImportCompletions = true,
+				},
+			},
+		},
+	})
+
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
+	local flake8 = require("efmls-configs.linters.flake8")
+	local black = require("efmls-configs.formatters.black")
+
+	-- configure efm server
+	lspconfig.efm.setup({
+		filetypes = {
+			"lua",
+			"python",
+		},
+		init_options = {
+			documentFormatting = true,
+			documentRangeFormatting = true,
+			hover = true,
+			documentSymbol = true,
+			codeAction = true,
+			completion = true,
+		},
+		settings = {
+			languages = {
+				lua = { luacheck, stylua },
+				python = { flake8, black },
+			},
+		},
+	})
+
+	-- Format on save
+	local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = lsp_fmt_group,
+		callback = function()
+			local efm = vim.lsp.get_active_clients({ name = "efm" })
+
+			if vim.tbl_isempty(efm) then
+				return
+			end
+
+			vim.lsp.buf.format({ name = "efm" })
+		end,
+	})
+end
+
+return {
+	"neovim/nvim-lspconfig",
+	config = config,
+	lazy = false,
+	dependencies = {
+		"windwp/nvim-autopairs",
+		"williamboman/mason.nvim",
+		"creativenull/efmls-configs-nvim",
+	},
+}
