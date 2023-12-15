6bacafb817d770b019feb1cba4a1747bea832fe2
v0x12 <olahnorbert1997@gmail.com>
2023-12-15 12:52:52 +0100

    Configuring tmux

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,11 +10,19 @@ keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", opts)
 keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
 keymap.set("n", "<leader>mf", ":NvimTreeFindFile<CR>", opts)
 
--- Pane Navigation
+-- Pane and Window Navigation
 keymap.set("n", "<C-h>", "<C-w>h", opts) -- Navigate left
 keymap.set("n", "<C-j>", "<C-w>j", opts) -- Navigate down
 keymap.set("n", "<C-k>", "<C-w>k", opts) -- Navigate up
 keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate right
+keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts) -- Navigate Left
+keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts) -- Navigate Down
+keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts) -- Navigate Up
+keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts) -- Navigate Right
+keymap.set("n", "<C-h>", "TmuxNavigateLeft",  opts) -- Navigate Left
+keymap.set("n", "<C-j>", "TmuxNavigateDown",  opts) -- Navigate Down
+keymap.set("n", "<C-k>", "TmuxNavigateUp",  opts) -- Navigate Up
+keymap.set("n", "<C-l>", "TmuxNavigateRight",  opts) -- Navigate Right
 
 -- Window management
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split vertically
@@ -28,4 +36,3 @@ keymap.set("v", ">", ">gv")
 -- Comments
 vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
 vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
-
