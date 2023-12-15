3ff336c7c7990a60478a0db8da2ea9140b18b8d7
v0x12 <olahnorbert1997@gmail.com>
2023-12-15 11:25:35 +0100

    adding transparency option

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,20 +1,21 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-      require("nvim-tree").setup {
-        view = {
-          width = {
-            min = 30,
-            max = 150,
-          }
-        }
-      }
-    end
-  }
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=None]])
+			require("nvim-tree").setup({
+				view = {
+					width = {
+						min = 30,
+						max = 150,
+					},
+				},
+			})
+		end,
+	},
 }
